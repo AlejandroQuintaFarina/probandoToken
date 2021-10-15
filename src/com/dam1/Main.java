4651815fac755f82a00f7836c74933a492a052fa
AlejandroQuintaFarina <aquintafarina@danielcastelao.org>
2021-10-15 09:58:38 +0200

    commit 1

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -1,8 +1,12 @@
 package com.dam1;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo 2");
     }
+
 }
